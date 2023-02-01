da3db0b2a5ac8ef4650da21c78fd807710303cde
Nicole Colgan <n.colgan3@nuigalway.ie>
2023-02-01 13:10:20 +0000

    first commit

---
+++ ibs-system-api/src/test/java/com/newIbsApp/ibs/IbsSystemApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.newIbsApp.ibs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IbsSystemApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
