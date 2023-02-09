dcb0d3ca65f4d27d1eef5c493aec2f7ed47c7d81
Nicole Colgan <n.colgan3@nuigalway.ie>
2023-02-09 12:03:59 +0000

    final UI design for sign up and register

--- ibs-system-api-front/src/App.js
+++ ibs-system-api-front/src/App.js
@@ -1,17 +1,25 @@
-import logo from "./logo.svg";
+
 import "./App.css";
 import SignUp from "./component/SignUp";
-import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import Container from "react-bootstrap/Container";
+import SignIn from "./component/SignIn";
 
 function App() {
   return (
     <Container fluid className="box " style={{ width: "100%", justifyContent: "flex-end" }}>
     <Col md={5} sm={5} className="column justify-content-center d-flex">
       <div className="">
-        <h1 style={{color: "#1a8cff"}}>The Gut Diary</h1>
-        <h5 style={{color: "#1a8cff"}}>Information about the app and our goals go here</h5>
+        <h1 
+        style={{
+          color: "#1a8cff",
+          fontSize: "60px",
+          fontWeight: 500
+          }}>THE GUT DIARY</h1>
+        <h4 
+        style={{
+          color: "#1a8cff",
+          fontWeight: 500}}>Information about the app and our goals go here</h4>
       </div>        
     </Col>
     <Col md={3} sm={3} className="column">
--- ibs-system-api-front/src/component/SignIn.js
+++ ibs-system-api-front/src/component/SignIn.js
@@ -1,12 +1,12 @@
 import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
 
-function SignUp() {
+function SignIn() {
   return (
     <div className="loginComponent"
     style={{
       width: "227px",
-      height: "350px",
+      height: "275px",
       paddingLeft: "50px",
       backgroundColor: "white",
       borderRadius: "25px"
@@ -21,46 +21,7 @@ function SignUp() {
           paddingLeft: "27px",
         }}>
         <h1
-        style={{fontWeight: 500}}>Sign Up</h1>
-      </div>
-      <div
-        style={{
-          display: "flex",
-          flexDirection: "column",
-          alignItems: "flex-start",
-        }}
-      >
-        <label className="text-gray-600 text-sm">First Name</label>
-        <div style={{ display: "flex" }}>
-          <input
-          style={{
-            outline: "1px solid #4da6ff",
-            borderRadius: "25px",
-
-          }}
-            type="text"
-            name="firstName"
-            className="h-10 w-96 border mt-5 py-10"
-          ></input>
-        </div>
-      </div>
-      <div
-        style={{
-          display: "flex",
-          flexDirection: "column",
-          alignItems: "flex-start",
-        }}
-      >
-        <label className="block text-gray-600 text-sm px-10">Last Name</label>
-        <div style={{ display: "flex" }}>
-          <input
-          style={{outline: "1px solid #4da6ff",borderRadius: "25px",
-        }}
-            type="text"
-            name="lastName"
-            className="h-10 w-96 border mt-2 px-2 py-2"
-          ></input>
-        </div>
+        style={{fontWeight: 500}}>Sign In</h1>
       </div>
       <div
         style={{
@@ -106,10 +67,14 @@ function SignUp() {
         }}
         className="px-6">
         
-<button className="submitRegistrationButton" style={{borderRadius: "25px"}}>Submit</button>
+<button className="submitRegistrationButton"
+style={{borderRadius: "25px"}}>Submit</button>
       </div>
+      <a style={{fontWeight: 400,
+      fontSize: "12px",
+      paddingLeft: "30px"}}>Forgot password?</a>
     </div>
   );
 }
 
-export default SignUp;
+export default SignIn;
