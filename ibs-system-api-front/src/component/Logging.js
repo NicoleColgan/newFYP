51cad55a94c07bfda852603ee8d2591bfe9da38f
Nicole Colgan <n.colgan3@nuigalway.ie>
2023-02-15 13:07:07 +0000

    Adding routes and logging page

---
+++ ibs-system-api-front/src/component/Logging.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Container from "react-bootstrap/Container";
+
+const Logging = () => {
+  return (
+    <Container fluid className="box " style={{ width: "100%", justifyContent: "flex-end" }}>
+    
+</Container>
+  )
+}
+
+export default Logging
