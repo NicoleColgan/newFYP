f8d51da12a6b7cbe35e16d734ac9a7901ff55aa5
Nicole Colgan <n.colgan3@nuigalway.ie>
2023-02-15 18:45:16 +0000

    changes to logging page

--- ibs-system-api-front/src/App.js
+++ ibs-system-api-front/src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import {BrowserRouter, Routes, Route} from "react-router-dom"
 import Landing from './component/Landing';
 import Logging from './component/Logging';
+
 //components (App) are used to render
 //you can only return one parent so if you wrap them in empty tags, you can return more than 1
 //The way routing works is that you show different components based on the different routes that you have defined
--- ibs-system-api-front/src/component/Logging.js
+++ ibs-system-api-front/src/component/Logging.js
@@ -1,12 +1,49 @@
-import React from 'react'
+import React from "react";
+import { Col } from "react-bootstrap";
 import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
 
 const Logging = () => {
   return (
-    <Container fluid className="box " style={{ width: "100%", justifyContent: "flex-end" }}>
-    
-</Container>
-  )
-}
+    <Container fluid className="loggingBox">
+  <Row style={{ height: '20vh' }}>rowhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhffffffffffffffffffffffffffffffffffffffffffffffffffffff</Row>
 
-export default Logging
+    <div style={{
+        height: '53.7vh',
+        backgroundColor: "#8CD9CF",
+        width: '212vh',
+        borderRadius: "25px", opacity: "80%" 
+    }}>
+  <Row className="loggingButtonsContainer">
+    <div style={{
+        paddingLeft: "10vh",
+        paddingTop: "5vh",
+        paddingRight: "10vh",
+        paddingBottom: "5vh",
+    }}className="loggingButtons">
+      <div>
+        <button className="loggingButton">Physical Symtom</button>
+        <span className="buttonSpace"></span>
+        <button className="loggingButton">Bowel Movements</button>
+      </div>
+      <div >
+        <button className="loggingButton">Appetite</button>
+        <span className="buttonSpace"></span>
+<button className="loggingButton">Wellbeing & Exercise</button>
+      </div>
+      <div>
+        <button className="loggingButton">Food & Supplements</button>
+        <span className="buttonSpace"></span>
+<button className="loggingButton">Other</button>
+      </div>
+    </div>
+  </Row>
+  </div>
+</Container>
+
+
+
+  );
+};
+
+export default Logging;
