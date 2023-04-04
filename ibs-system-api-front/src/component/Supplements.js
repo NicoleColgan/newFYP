33433d6f7402a57201a0c067eb0958c23a3a509c
Nicole Colgan <n.colgan3@nuigalway.ie>
2023-04-04 16:51:27 +0100

    All post logs working

--- ibs-system-api-front/src/component/Supplements.js
+++ ibs-system-api-front/src/component/Supplements.js
@@ -1,11 +1,48 @@
-import React from "react";
-import { useState } from "react";
+import React from 'react'
+import { useState, useEffect } from 'react';
+import LogService from "../services/LogService";
 
 const TriggerFood = (props) => {
-  const handleCloseClick = () => {
-    //passed the function to this compopnent
-    props.onClose();
+  const today = new Date();
+  const year = today.getFullYear();
+  const month = String(today.getMonth() + 1).padStart(2, '0');
+  const day = String(today.getDate()).padStart(2, '0');
+  const todayStr = `${year}-${month}-${day}`;
+  
+  async function handleCloseClick() {
+
+    var text = document.getElementById("textArea").value;
+    //check if any buttons are blue because that means we want to log something
+    if(text!=""){
+      //there is something to log
+      //if they selected physical symptom, they can have differerent data
+      //need to wait for the logData to be added before setting the log and posting it to the db
+      try {
+        await props.setLog({
+            userId: JSON.parse(localStorage.getItem("token")).id,
+            date: todayStr,
+            logType: "Supplements",
+            logDataEntities: [{ data: text }]
+          });
+        props.onClose();
+      } catch (error) {
+        console.error(error);
+      }
+    }
   };
+
+  useEffect(() => {
+    const saveLogAndSetLogData = async () => {
+      if (props.log.userId !== "" && props.log.date !== "" && props.log.logType !== "" && props.log.logDataEntities !== "") {
+        const loggedItem = await LogService.saveLog(props.log);
+        //have to convert logged item into a string because locaStorage holds strings
+        localStorage.setItem("logged", JSON.stringify(loggedItem.data));
+      }
+    }
+    saveLogAndSetLogData();
+  }, [props.log]);
+  
+ 
   const [button1Color, setButton1Color] = useState("#8CD9CF"); //green
   function handleSymptom1ButtonClicked() {
     //if any of the other buttons are clicked, turn them back to green
@@ -96,7 +133,7 @@ const TriggerFood = (props) => {
             paddingTop: "10px",
           }}
         >
-          <textarea
+          <textarea id="textArea"
             style={{
               width: "450px",
               height: "100px",
